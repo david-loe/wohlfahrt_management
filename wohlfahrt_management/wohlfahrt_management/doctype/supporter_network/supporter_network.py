b367d3538ab6ebe8316599be77052e4981dc2d8c
david-loe <56305409+david-loe@users.noreply.github.com>
2025-02-27 16:01:25 +0000

    adding Experiences and Networks

---
+++ wohlfahrt_management/wohlfahrt_management/doctype/supporter_network/supporter_network.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, david-loe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SupporterNetwork(Document):
+	pass
